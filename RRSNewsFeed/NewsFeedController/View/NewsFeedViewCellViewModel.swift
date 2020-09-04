16626ebcca3319e2c1f70ceae59e64cb67b97708
Sergey <whteahhtude@gmail.com>
2020-09-04 23:19:39 +0300

    cell and updating

---
+++ RRSNewsFeed/NewsFeedController/View/NewsFeedViewCellViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  NewsFeedViewCellViewModel.swift
+//  RRSNewsFeed
+//
+//  Created by Sergey berdnik on 04.09.2020.
+//  Copyright © 2020 Sergey berdnik. All rights reserved.
+//
+
+import Foundation
