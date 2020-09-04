38febe5c6da1c69e1a91c19161d8be90fc10beb5
Sergey <whteahhtude@gmail.com>
2020-09-04 22:26:59 +0300

    comit before pods

---
+++ RRSNewsFeed/NewsFeedController/NewsFeedViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  NewsFeedViewModel.swift
+//  RRSNewsFeed
+//
+//  Created by Sergey berdnik on 04.09.2020.
+//  Copyright © 2020 Sergey berdnik. All rights reserved.
+//
+
+import Foundation
